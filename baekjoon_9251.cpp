4613c30ab1130c8ff384b282cd64f857bd3a2417
mji43 <74688902+mji43@users.noreply.github.com>
2021-05-01 15:14:54 +0900

    Update baekjoon_9251.cpp

--- baekjoon_9251.cpp
+++ baekjoon_9251.cpp
@@ -15,10 +15,10 @@ int main()
 	{
 		for (int j = 1; j <= B.size(); j++)
 		{
-			if (A[i - 1] == B[j - 1])
-				DP[i][j] = DP[i - 1][j - 1] + 1;
-			else
-				DP[i][j] = max(DP[i][j - 1], DP[i - 1][j]);
+			if (A[i - 1] == B[j - 1]) //case 1: Ai = Bj
+				DP[i][j] = DP[i - 1][j - 1] + 1; // lcs(Ai-1, Bj-1)+1
+			else //case 2: Ai !=Bj
+				DP[i][j] = max(DP[i][j - 1], DP[i - 1][j]); //max(lcs(Ai-1, Bj),lcs(Ai,Bj-1)
 		}
 	}
 	cout << DP[A.size()][B.size()] << '\n';
